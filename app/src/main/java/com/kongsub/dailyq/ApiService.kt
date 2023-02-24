1c3833de93d240144279a18cfddabd1385af72f6
kongsubin <qls0919@naver.com>
2023-02-24 17:03:58 +0900

    :sparkles: add timeout functions

--- app/src/main/java/com/kongsub/dailyq/ApiService.kt
+++ app/src/main/java/com/kongsub/dailyq/ApiService.kt
@@ -13,6 +13,7 @@ import com.kongsub.dailyq.api.adapter.LocalDateAdapter
 import com.kongsub.dailyq.api.converter.LocalDateConverterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 /* Retrofit 생성시, OkHttpClient, Gson, Converters 등 여러 객체를 생성하므로
    싱글톤 패턴으로 만들어 앱 전체에서 하나의 인스턴스를 공유하게 만듦 */
@@ -34,6 +35,11 @@ interface ApiService {
              */
 
             return builder
+                .connectTimeout(3, TimeUnit.SECONDS)    // TCP handshake 실패 시, 기본값 10초
+                .writeTimeout(10, TimeUnit.SECONDS)
+                // 서버와 연결 후 데이터 수신시 정해진 시간 초과할때 발생,
+                // 10초안에 데이터를 받아와야한다는 뜻이 아닌, 데이터를 읽어오는 각 작동의 간격이 10초를 초과하면 안됨.
+                .readTimeout(10, TimeUnit.SECONDS)  // 서버로 데이터를 보낼 때 발생
                 .addInterceptor(logging)
                 .build()
         }
